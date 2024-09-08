ea6cc7cbd593472cc131d290f8b1d625ec28c5d7
Indigo Nolan <indigogonolan@gmail.com>
2024-09-08 15:37:07 +0100

    ADD: list of winners

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 
 function personal(){
-    
-    document.getElementById("personal").style.display = "flex";
-    document.getElementById("personal").style.left = "10vw";
+    document.getElementById("personal").style.left = "5vw";
     document.getElementById("personal").style.opacity = "1";
     document.getElementById("background").style.filter = "brightness(0.2)"
 }
 
 window.onload = function(){
-
     document.getElementById("background").style.filter = "brightness(0.6)"
-
     personal();
 }
